cb327eeba90c3fa241deb3a566a4f062d566ca16
clm <275298711@qq.com>
2023-06-18 16:41:00 +0800

    更新接口

--- routes/index.js
+++ routes/index.js
@@ -25,5 +25,43 @@ router.get('/space', function(req, res, next) {
   
 });
 
+router.get('/energy',(req,res)=>{
+  res.send({
+    data:[
+      {
+        "x": "房间1",
+        "y": 60,
+        "s": "系列一",
+        "colorField": 100
+      },
+      {
+        "x": "房间2",
+        "y": 40,
+        "s": "系列一"
+      }
+    ]
+  })
+})
+
+router.get('/warning',(req,res)=>{
+  const t = Math.random()*10;
+  const result = t-6>0;
+  if(result){
+    return res.send({
+      data:{
+        "content": "warning 空间1温度过高"
+      }
+    })
+  }else{
+    return res.send({
+      data:{
+        "content": "no warning"
+      }
+    })
+  }
+})
+
+
+
 
 module.exports = router;
