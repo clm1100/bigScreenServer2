f0d0d34269304151ad42901f31eaf5af719fbbc2
clm <275298711@qq.com>
2023-06-18 21:46:19 +0800

    还原

--- routes/index.js
+++ routes/index.js
@@ -16,17 +16,17 @@ router.get('/space', function(req, res, next) {
   const id = req.query.id;
   if(id==1){
    return  res.send({
-      data:{ "content": `温度：${rand(20,35)}`+"\n湿度："+`${rand(50,60)}`+"\n当日能耗："+`${rand(40,50)}`}
+      data:{ "content": "温度：30\n湿度：60\n当日能耗：40"}
     })
   }
   if(id==2){
    return  res.send({
-    data:{ "content": `温度：${rand(20,35)}`+"\n湿度："+`${rand(50,60)}`+"\n当日能耗："+`${rand(40,50)}`}
+      data:{"content": "温度：30\n湿度：60\n当日能耗：40"}
     })
   }
 
   res.send({
-    data:{ "content": `温度：${rand(20,35)}`+"\n湿度："+`${rand(50,60)}`+"\n当日能耗："+`${rand(40,50)}`}
+    data:{ "content": "温度：99\n湿度：99\n当日能耗：99"}
   })
   
 });
