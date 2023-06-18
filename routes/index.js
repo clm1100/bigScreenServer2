ce1cc32c3c6696819f8192babdb45c2cd98d274a
clm <275298711@qq.com>
2023-06-18 21:33:04 +0800

    更新接口

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment');
+function rand(min,max){
+
+  return parseInt(Math.random()*(max-min+1)+min);
+  
+  }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,17 +16,17 @@ router.get('/space', function(req, res, next) {
   const id = req.query.id;
   if(id==1){
    return  res.send({
-      data:{ "content": "温度：30\n湿度：60\n当日能耗：40"}
+      data:{ "content": `温度：${rand(20,35)}\n湿度：${rand(50,60)}\n当日能耗：${rand(40,50)}`}
     })
   }
   if(id==2){
    return  res.send({
-      data:{"content": "温度：30\n湿度：60\n当日能耗：40"}
+    data:{ "content": `温度：${rand(20,35)}\n湿度：${rand(50,60)}\n当日能耗：${rand(40,50)}`}
     })
   }
 
   res.send({
-    data:{ "content": "温度：99\n湿度：99\n当日能耗：99"}
+    data:{ "content": `温度：${rand(20,35)}\n湿度：${rand(50,60)}\n当日能耗：${rand(40,50)}`}
   })
   
 });
@@ -59,6 +65,54 @@ router.get('/warning',(req,res)=>{
       }
     })
   }
+})
+
+router.get('/average',(req,res)=>{
+
+  const currentHours = moment().format("H");
+  const t = moment().subtract(1,'hour').format("H");
+  console.log(t)
+
+  const arr = [];
+
+  for (let i = 8; i > 0; i--) {
+    const element = i;
+    const t = moment().subtract(i,'hour').format("H");
+    arr.push(t);
+    
+  }
+  console.log(arr);
+
+  let result = [];
+  arr.forEach((ele)=>{
+    console.log(ele);
+    result=result.concat([
+      {
+        "x": `${ele}时`,
+        "y": rand(18,36),
+        "s": "房间1-温度"
+      },
+      {
+        "x": `${ele}时`,
+        "y": rand(50,60),
+        "s": "房间1-湿度度"
+      },
+      {
+        "x": `${ele}时`,
+        "y": rand(17,35),
+        "s": "房间2-温度"
+      },
+      {
+        "x": `${ele}时`,
+        "y": rand(55,65),
+        "s": "房间2-湿度度"
+      },
+    ])
+  })
+
+  res.send({
+    data:result
+  })
 })
 
 
