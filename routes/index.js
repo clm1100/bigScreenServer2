9a09302dc24512b194a5c3234d9c768da51c31fe
clm <275298711@qq.com>
2023-06-18 10:10:13 +0800

    更新空间接口

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/space', function(req, res, next) {
+  const id = req.query.id;
+  if(id==1){
+   return  res.send({
+      "content": "温度：30\n湿度：60\n当日能耗：40"
+    })
+  }
+  if(id==2){
+   return  res.send({
+      "content": "温度：30\n湿度：60\n当日能耗：40"
+    })
+  }
+
+  res.send({
+    "content": "温度：99\n湿度：99\n当日能耗：99"
+  })
+  
+});
+
+
 module.exports = router;
